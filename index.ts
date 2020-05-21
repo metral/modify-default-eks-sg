c86972a5c4b2366cdb82e0f63a8e4152b8354fea
Mike Metral <1112768+metral@users.noreply.github.com>
2020-05-21 01:18:03 -0700

    init commit

---
+++ index.ts
@@ -0,0 +1,37 @@
+import * as aws from "@pulumi/aws";
+import * as eks from "@pulumi/eks";
+import * as pulumi from "@pulumi/pulumi";
+
+// Create an EKS cluster.
+const projectName = pulumi.getProject();
+const cluster = new eks.Cluster(projectName);
+export const kubeconfig = cluster.kubeconfig;
+
+// Get the results of the default security group created by AWS EKS.
+const defaultSecgroupNameGlob = `eks-cluster-sg-${projectName}-*`;
+const defaultSecgroupResult = pulumi.output(aws.ec2.getSecurityGroup({
+    filters: [{
+        name: "group-name",
+        values: [defaultSecgroupNameGlob],
+    }],
+    tags: cluster.core.cluster.name.apply(clusterName => ({
+        [`kubernetes.io/cluster/${clusterName}`]: "owned",
+    })),
+}, {async: true}));
+
+// Import the default security group into Pulumi.
+// https://www.pulumi.com/docs/guides/adopting/import/#adopting-existing-resources
+const defaultSecgroup = pulumi.all([
+    cluster.core.cluster.name,
+    cluster.core.vpcId,
+    defaultSecgroupResult,
+]).apply(([clusterName, vpcId, sgResult]) => {
+    return new aws.ec2.SecurityGroup("default-eks-sg", {
+        name: sgResult.name,
+        description: sgResult.description,
+        ingress: [{ protocol: "-1", fromPort: 0, toPort: 0, self: true}],
+        egress: [{ protocol: "-1", fromPort: 0, toPort: 0, cidrBlocks: ["0.0.0.0/0"]}],
+        tags: sgResult.tags,
+        vpcId,
+    }, {import: sgResult.id});
+});
